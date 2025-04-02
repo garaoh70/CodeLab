da6dca24a6b7f20bdbccab62c6dcee92ca00e7f6
Hima <tikal0122@outlook.jp>
2025-04-02 22:28:10 +0900

    Excel を開いてゴニョゴニョする

---
+++ OfficeSamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OfficeSamples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var excel = new Excel.Application();
+        excel.Visible = false;
+
+        var workbook = excel.Workbooks.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Book1.xlsx"));
+        var worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+
+        // 取り消し線のあるセルをクリアする
+        var range = worksheet.UsedRange;
+        foreach (var item in range.Cells)
+        {
+            if (item is not Excel.Range cell)
+                continue;
+
+            if (cell.Font.Strikethrough is bool strikethrough && strikethrough)
+            {
+                cell.ClearContents();
+            }
+        }
+
+        workbook.Close(false);
+        excel.Quit();
+
+        Marshal.ReleaseComObject(worksheet);
+        Marshal.ReleaseComObject(workbook);
+        Marshal.ReleaseComObject(excel);
+    }
+}
